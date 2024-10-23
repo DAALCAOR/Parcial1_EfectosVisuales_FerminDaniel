d426c680204a7f16a7349e53a2a0d39b18923780
Daniel Cáceres <167147119+DAALCAOR@users.noreply.github.com>
2024-10-23 08:34:31 -0300

    Avance camara de video

--- Assets/Scripts/Video_Camera_Controller.cs
+++ Assets/Scripts/Video_Camera_Controller.cs
@@ -2,17 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteAlways]
 public class Video_Camera_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Video_Camera_Controller Instance;
+
+    public bool spacePressed;
+    #region Singleton
+    public void Awake()
     {
-        
+        if(!Instance)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+    [Header("<color=blue>Color Adjustment</color>")]
+
+    [SerializeField] private Material _colAdjPP;
+
+    public Material ColAdjPP
+    {
+        get { return _colAdjPP; }
+        set { _colAdjPP = value; }
     }
 
-    // Update is called once per frame
-    void Update()
+    [Header("<color=red>Vignette</color>")]
+
+    [SerializeField] private Material _vignettePP;
+
+    public Material VignettePP
     {
-        
+        get { return _vignettePP; }
+        set { _vignettePP = value; }
     }
+
+    public float _vignetteBlur;
+    public float _vignetteScale;
+
+    private Animator _anim;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        SetVignetteBlur();
+        SetVignetteScale();
+    }
+
+    public void PostProcessState(bool isActive)
+    {
+        _anim.SetBool("isActive", isActive);
+        _anim.SetTrigger("onEnable");
+    }
+
+    public void SetVignetteScale()
+    {
+        _vignettePP.SetFloat("_VignetteScale", _vignetteScale);
+    }
+
+    public void SetVignetteBlur()
+    {
+        _vignettePP.SetFloat("_VignetteBlur", _vignetteBlur);
+    }
+
+    //public void ToogleOn()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        spacePressed = !spacePressed;
+    //    }
+    //}
 }
